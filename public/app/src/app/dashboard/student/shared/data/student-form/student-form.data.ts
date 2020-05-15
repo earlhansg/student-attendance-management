13e55069b7e2c6e0e6add86612254c6f5fdd600e
Earl Hans Genoso <earlhansg@gmail.com>
2020-05-15 21:28:40 +0800

    #11 Create data for forms to render dynamically

---
+++ public/app/src/app/dashboard/student/shared/data/student-form/student-form.data.ts
@@ -0,0 +1,33 @@
+export const studentFormData = {
+  ID: { label: 'ID', value: '', type: 'number', validators: { required: true, min: 8 } },
+  firstname: { label: 'Firstname', value: '', type: 'text', validators: { required: true }  },
+  lastname: { label: 'Lastname', value: '', type: 'text', validators: { required: true } },
+  gender: {
+    label: 'Gender',
+    value: 'M',
+    type: 'select',
+    options: [
+      {
+        label: 'Male', value: 'M'
+      },
+      {
+        label: 'Female', value: 'F'
+      }
+    ],
+  },
+  section: {
+    label: 'Section',
+    value: 1,
+    type: 'select',
+    options: [
+      {
+        label: 'Rizal', value: 1
+      },
+      {
+        label: 'Bonifacio', value: 2
+      }
+    ]
+  }
+};
+
+
